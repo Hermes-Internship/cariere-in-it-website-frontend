b117493d2ca5f64841335596ae1402173e5740e3
bianca-paula <bianca.paula2505@gmail.com>
2021-07-18 20:22:59 +0300

    Created nav bar with routes

--- hermes-cariere-in-it-website/src/app/app-routing.module.ts
+++ hermes-cariere-in-it-website/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ScheduleComponent } from './components/schedule/schedule.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component:MainPageComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'schedule', component: ScheduleComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'contact', component: ContactComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- hermes-cariere-in-it-website/src/app/app.module.ts
+++ hermes-cariere-in-it-website/src/app/app.module.ts
@@ -4,15 +4,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { AboutComponent } from './components/about/about.component';
+import { ScheduleComponent } from './components/schedule/schedule.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { RouterModule } from '@angular/router';
+import { MainPageComponent } from './components/main-page/main-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ScheduleComponent,
+    RegisterComponent,
+    ContactComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NoopAnimationsModule
+    NoopAnimationsModule,
+    RouterModule
+  ],
+  exports: [
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
