1a3ed8a8ef98dbf3709d7a837575e65a8163abd7
Komșa Attila <63514374+kms77@users.noreply.github.com>
2021-09-04 14:32:24 +0300

    new uodate

--- hermes-cariere-in-it-website/src/app/components/main-page/main-page.component.ts
+++ hermes-cariere-in-it-website/src/app/components/main-page/main-page.component.ts
@@ -9,17 +9,25 @@ import { Router } from '@angular/router';
 export class MainPageComponent implements OnInit {
 
   constructor(
-    private router:Router
+    private router:Router,
+    //public visibleList: boolean = false
   ) { }
-
   ngOnInit(): void {
+    //throw new Error('Method not implemented.');
   }
 
-  navigateToAboutPage(){
+
+  navigateToAboutPage(): void{
     this.router.navigateByUrl("/about");
   }
   
-  
-
-
+  makeResponsiveMenu(): void{
+    /*if(this.visibleList === false)
+    {
+      this.visibleList = true;
+    }
+    else{
+      this.visibleList = false;
+    }*/
+  }
 }
